72c28d1914dc9110839dc1156674b06ef862c9f9
Santiago Hyun <santiago.hyun@codescrum.com>
2019-09-27 11:19:54 -0500

    Fixing test

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,11 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import Pipeline = require('../lib/pipeline-stack');
+import { LambdaStack } from '../lib/lambda-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
+    const lambdaStack = new LambdaStack(app, 'MyTestLambdaStack');
+    const stack = new Pipeline.PipelineStack(app, 'MyTestStack', { lambdaCode: lambdaStack.lambdaCode});
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
