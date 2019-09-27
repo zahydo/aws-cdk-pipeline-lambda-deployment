34b9191acd4e6e2215d7adf33c7514a44154be09
Santiago Hyun <santiago.hyun@codescrum.com>
2019-09-27 09:50:45 -0500

    Initial configuration: Adding index.js to handler Lambda Function

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,9 +1,22 @@
-import cdk = require('@aws-cdk/core');
+import { App, Stack, StackProps } from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import codecommit = require('@aws-cdk/aws-codecommit');
+import codedeploy = require('@aws-cdk/aws-codedeploy');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import s3 = require('@aws-cdk/aws-s3');
 
-export class PipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+// we define a new props interface for it, PipelineStackProps. 
+// This extends the standard StackProps, and use it in its constructor signature. 
+// This is how clients of this class pass the Lambda code that the class needs.
+export interface PipelineStackProps extends StackProps {
+  readonly lambdaCode: lambda.CfnParametersCode;
+}
 
+export class PipelineStack extends Stack {
+  constructor(app: App, id: string, props?: StackProps) {
+    super(app, id, props);
+
+    const code = codecommit.Repository.fromRepositoryName(this, 'ImportedRepo', 'hello-world-lambda-function');
     // The code that defines your stack goes here
   }
 }
